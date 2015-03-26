c659018a184983d5627a3b63731fe4dbede72a77
dgaspari <daniel.gaspari@gmail.com>
2015-03-25 22:40:52 -0400

    Trying to desample audio input

--- pyrapt/pyrapt.py
+++ pyrapt/pyrapt.py
@@ -3,6 +3,7 @@ This module defines the Pyrapt class, which encapsulates an implementation of
 David Talkin's Robust Algorithm for Pitch Tracking (RAPT).
 """
 
+from scipy import signal
 from scipy.io import wavfile
 
 class Pyrapt(object):
@@ -31,4 +32,6 @@ class Pyrapt(object):
         #get audio input data using scipy.io wav reader:
         original_audio = wavfile.read(wavfile_path)
         downsampling_rate = Pyrapt.calculate_downsampling_rate(original_audio[0], 500) #2nd arg could be constant configurable - max Fzero
-        return downsampling_rate
+        #downsampled_audio = signal.resample(original_audio[1], int(downsampling_rate)) #decimate or resample options available here:
+        downsampled_audio = signal.decimate(original_audio[1], int(downsampling_rate)) #TODO: need details on lowpass fir filter
+        return downsampled_audio
