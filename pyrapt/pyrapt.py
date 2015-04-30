9ab20fd4d85bcd0522f5ab71d53a8848bdd0ffdb
dgaspari <daniel.gaspari@gmail.com>
2015-04-30 13:16:05 -0400

    Laying out NCCF formula basics. Needs to be readjusted, so actions are still commented out

--- pyrapt/pyrapt.py
+++ pyrapt/pyrapt.py
@@ -3,6 +3,7 @@ This module encapsulates the rapt function, which runs an implementation
 of David Talkin's Robust Algorithm for Pitch Tracking (RAPT).
 """
 
+import math
 import numpy
 from scipy import signal
 from scipy.io import wavfile
@@ -81,24 +82,44 @@ def nccf_first_pass(downsampled_audio, downsampling_rate, sample_rate):
     maximum_allowed_freq = 500
     minimum_allowed_freq = 50
     frame_step_size = 0.01
+    correlation_window_size = 0.0075
 
     # starting value for "k" in NCCF equation
-    shortest_lag_per_frame = downsampling_rate / maximum_allowed_freq
+    shortest_lag_per_frame = int(downsampling_rate / maximum_allowed_freq)
 
     # Value of "K" in NCCF equation
-    longest_lag_per_frame = downsampling_rate / minimum_allowed_freq
+    longest_lag_per_frame = int(downsampling_rate / minimum_allowed_freq)
 
     # Value of "z" in NCCF equation
-    samples_per_frame = frame_step_size * downsampling_rate
+    samples_per_frame = int(frame_step_size * downsampling_rate)
 
     # Value of "M" in NCCF equation
     max_frame_count = int(len(downsampled_audio) / samples_per_frame)
 
+    # Value of "n" in NCCF equation
+    num_samples_per_lag = int(correlation_window_size * downsampling_rate)
+
     lag_range = longest_lag_per_frame - shortest_lag_per_frame
 
+    # TODO: Re-read discussion of using double-precision arithmetic in 3.3
+    # TODO: adjust formula based on variations in 3.3
     candidates = numpy.zeros((max_frame_count, lag_range))
     for i in xrange(0, max_frame_count):
         for k in xrange(0, lag_range):
-            candidates[i][k] = 1
+            # summation of samples within frame * said sample + lag:
+            # value of "m" in NCCF equation:
+            start_sample = i * samples_per_frame
+            sample_sum = 0
+            squared_sum = 0
+            squared_sum_with_lag = 0
+            # TODO: debug through here, figure out array index issues
+            # for j in xrange(start_sample,
+            #                 start_sample + num_samples_per_lag - 1):
+            #     sample_sum += (downsampled_audio[j] * downsampled_audio[j + k])
+            #     squared_sum += downsampled_audio[j] ** 2
+            #     squared_sum_with_lag += downsampled_audio[j + k] ** 2
+            # divided by sqrt e_m * e_m + lag
+            # candidates[i][k] = (sample_sum /
+            #                     math.sqrt(squared_sum * squared_sum_with_lag))
 
     return candidates
