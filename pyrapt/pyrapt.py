3e55035c8eec0e4c08f6c5cdd46c6673dad253f2
dgaspari <daniel.gaspari@gmail.com>
2015-06-06 23:00:48 -0400

    defined second pass nccf function with some of the necessary structure. now returning max correlation val from first pass of nccf (highest correlation is used to limit which lags are checked each frame)

--- pyrapt/pyrapt.py
+++ pyrapt/pyrapt.py
@@ -24,13 +24,15 @@ def rapt(wavfile_path):
 
     # NCCF (normalized cross correlation function) - identify F0 candidates
     # TODO: Determine if we want to preprocess audio before NCCF
-    first_pass = _first_pass_nccf(downsampled_audio, downsampling_rate)
-    # period_candidates = _second_pass_nccf(audio_sample, sample_rate)
+    first_pass, max_cor = _first_pass_nccf(downsampled_audio, downsampling_rate)
+    # first_pass is the theta_i,k and max_cor is theta_max from the results
+    # period_candidates = _second_pass_nccf(audio_sample, sample_rate,
+    #                                        first_pass, max_cor)
 
     # Dynamic programming - determine voicing state at each period candidate
 
     # return output of nccf for now
-    return first_pass
+    return first_pass, max_cor
 
 
 def _get_audio_data(wavfile_path):
@@ -105,8 +107,86 @@ def _first_pass_nccf(audio_input, sample_rate):
     samples_correlated_per_lag = int(correlation_window_size * sample_rate)
 
     # starting value for "k" in NCCF equation
-    # NOTE: THIS IS ONLY MEANT FOR FIRST PASS, OTHERWISE k STARTS AT ZERO
     shortest_lag_per_frame = int(sample_rate / maximum_allowed_freq)
+
+    # Value of "K" in NCCF equation
+    longest_lag_per_frame = int(sample_rate / minimum_allowed_freq)
+
+    # Value of "z" in NCCF equation
+    samples_per_frame = int(frame_step_size * sample_rate)
+
+    # Value of "M-1" in NCCF equation
+    max_frame_count = (int(float(len(audio_input)) / float(samples_per_frame)
+                       - 1))
+
+    # Difference between "K-1" and starting value of "k"
+    lag_range = (longest_lag_per_frame - 1) - shortest_lag_per_frame
+
+    # Value of theta_max in NCCF equation
+    max_correlation_val = 0.0
+
+    # TODO: Re-read discussion of using double-precision arithmetic in 3.3
+    candidates = numpy.zeros((max_frame_count, lag_range))
+    for i in xrange(0, max_frame_count):
+        for k in xrange(0, lag_range):
+            current_lag = k + shortest_lag_per_frame
+
+            # determine if the current lag value causes us to go past the
+            # end of the audio sample - if so - skip and set val to 0
+            if ((current_lag + (samples_correlated_per_lag - 1)
+                    + (i * samples_per_frame)) >= audio_input.size):
+                candidates[i][k] = 0
+                continue
+
+            samples = 0
+            for j in xrange(0, samples_correlated_per_lag - 1):
+                correlated_samples = _get_sample(audio_input, i, j,
+                                                 samples_per_frame,
+                                                 samples_correlated_per_lag,
+                                                 longest_lag_per_frame)
+                samples_for_lag = _get_sample(audio_input, i, j + current_lag,
+                                              samples_per_frame,
+                                              samples_correlated_per_lag,
+                                              longest_lag_per_frame)
+                samples += correlated_samples * samples_for_lag
+
+            denominator = _get_nccf_denominator_val(audio_input, i, 0,
+                                                    samples_per_frame,
+                                                    samples_correlated_per_lag,
+                                                    longest_lag_per_frame)
+
+            denominator *= _get_nccf_denominator_val(audio_input, i,
+                                                     current_lag,
+                                                     samples_per_frame,
+                                                     samples_correlated_per_lag,
+                                                     longest_lag_per_frame)
+            candidates[i][k] = float(samples) / float(denominator)
+            if candidates[i][k] > max_correlation_val:
+                max_correlation_val = candidates[i][k]
+
+    return candidates, max_correlation_val
+
+
+def _second_pass_nccf(audio_input, sample_rate, first_pass):
+    # TODO: Make these optional params that default to below values:
+    # Value of "F0_max" in NCCF equation:
+    # maximum_allowed_freq = 500
+    # Value of "F0_min" in NCCF equation:
+    minimum_allowed_freq = 50
+    # Value of "t" in NCCF equation:
+    frame_step_size = 0.01
+    # Value of "w" in NCCF equation:
+    correlation_window_size = 0.0075
+    # Value of "AFACT" in NCCF equation:
+    absolute_frequency_addition = 10000
+
+    # Value of "n" in NCCF equation:
+    samples_correlated_per_lag = int(correlation_window_size * sample_rate)
+
+    # starting value for "k" in NCCF equation
+    # NOTE: THIS IS ONLY MEANT FOR FIRST PASS, OTHERWISE k STARTS AT ZERO
+    # shortest_lag_per_frame = int(sample_rate / maximum_allowed_freq)
+    shortest_lag_per_frame = 0
 
     # Value of "K" in NCCF equation
     longest_lag_per_frame = int(sample_rate / minimum_allowed_freq)
@@ -156,11 +236,10 @@ def _first_pass_nccf(audio_input, sample_rate):
                                                      samples_per_frame,
                                                      samples_correlated_per_lag,
                                                      longest_lag_per_frame)
-
+            denominator += math.sqrt(absolute_frequency_addition)
             candidates[i][k] = float(samples) / float(denominator)
 
     return candidates
-
 
 # TODO: at some point start_sample is FURTHER than last_sample_in_frame
 # (when calculating denominator). Why is that? we use m + n - 1 for
@@ -173,6 +252,7 @@ def _first_pass_nccf(audio_input, sample_rate):
 # last_sample_in_frame: 3314 <<< heres the problem
 # the mean_for_frame val seems fine, but sum_frame_samples fails because we
 # try and calc sum of input from 3333 to 3314
+
 
 # TODO: Figure out array out of bounds error
 def _get_sample(audio_input, frame_index, correlation_index, samples_per_frame,
