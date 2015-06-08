5f682ecd02964277b07633450a43c7c6b429560c
dgaspari <daniel.gaspari@gmail.com>
2015-06-07 23:44:15 -0400

    Fixed unit tests

--- tests/test_nccf_methods.py
+++ tests/test_nccf_methods.py
@@ -15,16 +15,16 @@ class TestNccfMethods(TestCase):
         # TODO: This is with default params. Do it with passed in ones as well
         sample_rate = 1000
         audio_data = numpy.zeros(1000)
-        candidates = pyrapt._first_pass_nccf(audio_data, sample_rate)
-        self.assertEqual((100, 18), candidates.shape)
+        candidates, max_corr = pyrapt._first_pass_nccf(audio_data, sample_rate)
+        self.assertEqual((99, 17), candidates.shape)
 
     def test_get_signal(self):
         # sample_rate = 1000
         audio_data = numpy.ones(1000)
         audio_data2 = numpy.zeros(1000)
-        signal = pyrapt._get_samples(audio_data, 0, 0, 10, 8, 20)
-        self.assertEqual(1.0, signal)
-        signal = pyrapt._get_samples(audio_data2, 0, 0, 10, 8, 20)
+        signal = pyrapt._get_sample(audio_data, 0, 0, 10, 8, 20)
+        self.assertEqual(0.125, signal)
+        signal = pyrapt._get_sample(audio_data2, 0, 0, 10, 8, 20)
         self.assertEqual(0, 0, signal)
-        signal = pyrapt._get_samples(audio_data, 5, 0, 10, 8, 20)
-        self.assertEqual(1.0, signal)
+        signal = pyrapt._get_sample(audio_data, 5, 0, 10, 8, 20)
+        self.assertEqual(0.125, signal)
