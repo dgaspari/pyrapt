46fc6016c6d312eed7f81fb8bdb51b1648710a59
dgaspari <daniel.gaspari@gmail.com>
2015-06-08 22:27:32 -0400

    Adding test class for main function

---
+++ tests/test_rapt.py
@@ -0,0 +1,19 @@
+"""
+Unit tests for main methods used by rapt
+"""
+from unittest import TestCase
+from mock import patch
+
+import numpy
+
+from pyrapt import pyrapt
+
+
+class TestMainRaptMethods(TestCase):
+
+    @patch('pyrapt.pyrapt._get_audio_data')
+    def test_rapt_control_flow(self, mock_getaudio):
+        mock_getaudio.return_value = 44100, numpy.full(73641, 1.0)
+        x, y = pyrapt.rapt('test.wav')
+        mock_getaudio.called_once_with('test.wav')
+        self.assertTrue(y < 0.001)
