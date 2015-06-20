b53ccd142ddb90cf6e5d87f8a16e7856c9227827
dgaspari <daniel.gaspari@gmail.com>
2015-06-19 21:16:32 -0400

    Fixed top-level rapt test with new mock method calls

--- tests/test_rapt.py
+++ tests/test_rapt.py
@@ -12,8 +12,13 @@ from pyrapt import pyrapt
 class TestMainRaptMethods(TestCase):
 
     @patch('pyrapt.pyrapt._get_audio_data')
-    def test_rapt_control_flow(self, mock_getaudio):
-        mock_getaudio.return_value = 44100, numpy.full(73641, 5.0)
-        x, y = pyrapt.rapt('test.wav')
-        mock_getaudio.called_once_with('test.wav')
-        self.assertTrue(y < 1)
+    @patch('pyrapt.pyrapt._get_downsampled_audio')
+    @patch('pyrapt.pyrapt._run_nccf')
+    def test_rapt_control_flow(self, mock_audio, mock_downsample, mock_nccf):
+        mock_audio.return_value = (44100, numpy.full(100, 3.0))
+        with patch('pyrapt.pyrapt._get_downsampled_audio') as mock_downsample:
+            mock_downsample.return_value = (2004, numpy.full(10, 2.0))
+            with patch('pyrapt.pyrapt._run_nccf') as mock_get_nccf:
+                mock_get_nccf.return_value = (numpy.zeros((10, 10)), 0.5)
+                x, y = pyrapt.rapt('test.wav')
+                self.assertEqual(y, 0.5)
