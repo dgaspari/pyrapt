116bd5919169f4bba39cb0975f864a3740974b28
dgaspari <daniel.gaspari@gmail.com>
2015-08-30 22:44:07 -0400

    plotting freq map on top of example wav file amplitude

--- visualization/plot_example.py
+++ visualization/plot_example.py
@@ -1,10 +1,18 @@
-from pyrapt import pyrapt
-from bokeh.plotting import figure, output_file
-# from bokeh.plotting import show
+from pyrapt import pyrapt, raptparams
+from bokeh.plotting import figure, output_file, save
+import numpy
+params = raptparams.Raptparams()
+example_audio = pyrapt._get_audio_data('example.wav')
+samples_per_frame = int(example_audio[0] * params.frame_step_size)
 y = pyrapt.rapt('example.wav')
 x = range(0, len(y))
+y = numpy.array(y)
+x = numpy.array(x)
+x = x * samples_per_frame
 output_file('visualization/output/example_plot.html', title='example.wav f0')
-p = figure(title='freq of example.wav', x_axis_label='frame #',
-           y_axis_label='freq (hz)')
-p.line(x, y, legend='example.wav f0', line_width=2)
-# show(p)
+p = figure(title='freq of example.wav', x_axis_label='x',
+           y_axis_label='y')
+p.line(range(0, len(example_audio[1])), example_audio[1],
+       legend='amp of example.wav', line_width=1, line_color='orange')
+p.line(x, y, legend='example.wav f0', line_width=2, line_color='blue')
+save(p)
