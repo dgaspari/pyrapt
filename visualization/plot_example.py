52714b061cc57b7c7dc7d46a1ecc9a9f178d75fe
dgaspari <daniel.gaspari@gmail.com>
2015-09-12 14:22:44 -0400

    removed metadata from example wavs so scipy could read them, cut off empty space on some wavs to help them get read properly (1-3 raise errors in my code tho). added readme to describe what samples have. updated plot_example to allow easy modifiability

--- visualization/plot_example.py
+++ visualization/plot_example.py
@@ -3,10 +3,17 @@ from bokeh.plotting import figure, output_file, save
 # from bokeh.models import ColumnDataSource
 import numpy
 from matplotlib import mlab
+
+show_freq = True
+show_nccf = True
+show_amp = True
+audio_file = 'newsamples/example0.wav'
+
+
 params = raptparams.Raptparams()
-example_audio = pyrapt._get_audio_data('example.wav')
+example_audio = pyrapt._get_audio_data(audio_file)
 samples_per_frame = int(example_audio[0] * params.frame_step_size)
-results = pyrapt.rapt_with_nccf('example.wav')
+results = pyrapt.rapt_with_nccf(audio_file)
 nccf_results = results[0]
 nccf_cands = []
 z = []
@@ -26,8 +33,11 @@ spec = mlab.specgram(example_audio[1])
 output_file('visualization/output/example_plot.html', title='example.wav f0')
 p = figure(title='freq of example.wav', x_axis_label='x',
            y_axis_label='y')
-p.line(range(0, len(example_audio[1])), example_audio[1],
-       legend='amp of example.wav', line_width=1, line_color='orange')
-p.line(x, y, legend='example.wav f0', line_width=2, line_color='blue')
-p.circle(z, nccf_cands, legend='nccf candidates', color='red', size=1)
+if show_amp:
+    p.line(range(0, len(example_audio[1])), example_audio[1],
+           legend='amp of example.wav', line_width=1, line_color='orange')
+if show_freq:
+    p.line(x, y, legend='example.wav f0', line_width=2, line_color='blue')
+if show_nccf:
+    p.circle(z, nccf_cands, legend='nccf candidates', color='red', size=1)
 save(p)
